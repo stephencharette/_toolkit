8a22ac093e039313b1c303055696fdb8675a000e
Stephen <stephen.charette@outlook.com>
2023-06-22 21:02:03 -0400

    try to fix path to index.html

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.delete(
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "./client/public/index.html"),
+    path.join(__dirname, "/client/public/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
